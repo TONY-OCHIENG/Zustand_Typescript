16b4cfa087e4a1056255a7763e1aed9ca93b882a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-21 08:43:01 +0300

    Refactoring count

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Button } from './@/components/ui/button'
-import { create } from 'zustand'
+import Count from './Count'
 
-const useStore = create<{
-  count: number;
-  increment: () => void;
-  decrement: () => void;
-}>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({count: state.count + 1})),
-  decrement: () => set((state) => ({count: state.count - 1}))
-}))
+
 
 function App() {
-  const { increment, decrement, count} = useStore()
   return (
-    <div className='h-[100vh]'>
-          <Button onClick={decrement}>Minus</Button>
-          {count}
-          <Button onClick={increment}>Add</Button>  
+    <div>
+      <Count/>
     </div>
   )
 }
---
+++ src/Count.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { create } from 'zustand'
+import { Button } from './@/components/ui/button'
+import { useStore } from './store/store'
+
+
+
+function Count() {
+    const { count,increment,decrement } = useStore()
+  return (
+    <div className='flex justify-center items-center h-[100vh] flex-col'>
+        <h1>{count}</h1>
+        <div className='flex gap-10'>
+            <Button onClick={decrement}>Decrement</Button>
+            <Button onClick={increment}>Increment</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Count
